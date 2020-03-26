f18184edec6b141dd93cb673599487cded49d93c
Lloyd Keijzer <l.keijzer@wearetriple.com>
2020-03-26 10:04:21 +0100

    MacOS support

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     platforms: [
         .iOS(.v10),
         .tvOS(.v9),
-        .watchOS(.v2)
+        .watchOS(.v2),
+        .macOS(.v10_12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
